c822e26d7b5858ba796fdac434247ec21c7cae55
paperplane030 <dinosaur0023@gmail.com>
2022-08-24 10:05:13 +0800

    Update 2022-08-24T02:05:06.559Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
+"use strict";
+
 $(function () {
   console.log('Hello Bootstrap5');
   $('#modal-expand').on('click', function (e) {
     $('#modal-expand').toggleClass('active');
   });
 });
+//# sourceMappingURL=all.js.map
